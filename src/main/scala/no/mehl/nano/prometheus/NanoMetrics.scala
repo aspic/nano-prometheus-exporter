4b62b350c48d8f7bf116f0bc7ce34c1950d8edad
Kjetil Mehl <kjetil@mehl.no>
2019-05-24 23:23:01 +0200

    Some gauges

--- src/main/scala/no/mehl/nano/prometheus/NanoMetrics.scala
+++ src/main/scala/no/mehl/nano/prometheus/NanoMetrics.scala
@@ -9,13 +9,10 @@ import org.http4s.client.Client
 class NanoMetrics[F[_]: Applicative: Sync](c: CollectorRegistry, client: Client[F], config: Config[F]) {
 
   val votingWeightOfTotal: Gauge =
-    Gauge.build("voting_weight_of_total_supply", "Percent of total voting weight").create()
-  c.register(votingWeightOfTotal)
-  val votingWeightGauge: Gauge = Gauge.build("voting_weight", "Voting weight for this node").create()
-  c.register(votingWeightGauge)
+    Gauge.build("voting_weight_of_total_supply", "Percent of total voting weight").create().register(c)
+  val votingWeightGauge: Gauge = Gauge.build("voting_weight", "Voting weight for this node").create().register(c)
 
-  val representativesCount: Gauge = Gauge.build("representatives_count", "Counts all representatives").create()
-  c.register(representativesCount)
+  val representativesCount: Gauge = Gauge.build("representatives_count", "Counts all representatives").create().register(c)
 
   val representativesWeight: Gauge = Gauge
     .build("top_representatives_weight", "5 largest representatives and their weight")
@@ -23,15 +20,13 @@ class NanoMetrics[F[_]: Applicative: Sync](c: CollectorRegistry, client: Client[
       "rep"
     )
     .create()
-  c.register(representativesWeight)
+    .register(c)
 
-  val rrRepsCount: Gauge = Gauge.build("rebroadcasting_nodes","Count of all rebroadcasting nodes").create()
-  c.register(rrRepsCount)
-
+  val rrRepsCount: Gauge = Gauge.build("rebroadcasting_nodes","Count of all rebroadcasting nodes").create().register(c)
   val almostThere: Gauge = Gauge.build("close_nodes", "Count 100 closest nodes to 0.001% voting weight")
     .labelNames("rep")
     .create()
-  c.register(almostThere)
+    .register(c)
 
   def update(): fs2.Stream[F, Unit] =
     for {
